d89e1d774a3b7c904d02443fadcf85ebb8611700
Talha Tariq <talhacoder01@gmail.com>
2022-10-11 23:53:05 +0500

    Day 3 Challange Completed

--- solutions/day-03/my-app/src/App.js
+++ solutions/day-03/my-app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from './logo.svg'
+import './App.css'
+import image from './assest/talha.jpg'
+import skills from './data/skills'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='image'>
+        <img src={image} alt='talha' />
+      </div>
+      <h1>Talha Tariq</h1>
+      <p>Senior Developer, Pakistan</p>
+      <div>
+        <h3>Skills</h3>
+        <div className='buttons'>
+          {skills.map((skill) => (
+            <button className='btn'>{skill}</button>
+          ))}
+        </div>
+      </div>
+      <p>Joined on Aug 30, 2022</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ solutions/day-03/my-app/src/data/skills.js
@@ -0,0 +1,28 @@
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Node',
+  'Express',
+  'MongoDB',
+  'SQL',
+  'Python',
+
+  'Git',
+  'GitHub',
+  'Heroku',
+  'Netlify',
+  'VS Code',
+  'Postman',
+  'Figma',
+  'Adobe XD',
+  'Java',
+  'C++',
+  'C#',
+  'C',
+  'PHP',
+  'Bootstrap',
+]
+
+export default skills
